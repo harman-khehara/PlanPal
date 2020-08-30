f63192c1b2ed41cb83b11acb91782feeb4f991e8
Harman Khehara <65868954+harman-khehara@users.noreply.github.com>
2020-08-30 02:01:31 -0400

    completed UI of page

--- CreateSchedule1.dart
+++ CreateSchedule1.dart
@@ -77,13 +77,62 @@ class _CreateSchedule1State extends State<CreateSchedule1> {
       ]),
       body: Center(
           child: Column(children: [
-        Expanded(
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 25.0, 16.0, 16.0),
+              child: SizedBox(
+                height: 340.0,
+                  width: 380.0,
+                  child: Material(
+                      borderRadius: BorderRadius.circular(24.0),
+                      color: Colors.lightBlue[100],
+                      elevation: 14.0,
+                      shadowColor: Colors.blueGrey,
+                      child: Container(
+                          padding: const EdgeInsets.fromLTRB(25.0, 20.0, 25.0, 13.0),
+                          child: Column(
+                            children: [
+                              Text('Begin by entering a name for the task you wish to work on for the day. '
+                                  'Next to the name of the task, enter the amount of hours you wish to spend on that task. To add more tasks, '
+                                  "click the + button near the bottom of the screen. Click the 'Done' button on the bottom of the screen once you have added "
+                                  "all tasks for the day; proceed by clicking 'Next'.",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(fontSize: 15, color: Colors.black)),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(0, 20.0, 0, 13.0),
+                                  child: Text('NOTE:',
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black)
+                              )
+                              ),
+                              Padding(
+                                  padding: const EdgeInsets.fromLTRB(6.0, 2.0, 6.0, 5.0),
+                                  child: Text('1.   The total time you wish to spend on all tasks for the day must not exceed 12 hours.',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.black)
+                                  )
+                              ),
+                              Padding(
+                                  padding: const EdgeInsets.fromLTRB(6.0, 2.0, 6.0, 5.0),
+                                  child: Text(' 2.   Once you have chosen the amount of time you wish to spend on a task, it cannot be changed.',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.black)
+                                  )
+                              )
+                            ]
+                          )
+                          ))),
+            ),
+
+
+            Expanded(
           child: Container(
               padding: const EdgeInsets.all(16.0),
               child: Material(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(24.0),
+                ),
                 color: Colors.white,
                 elevation: 14.0,
-                borderRadius: BorderRadius.circular(24.0),
                 shadowColor: Colors.blueGrey,
                 child: new ListView(
                   children: _tasks,
